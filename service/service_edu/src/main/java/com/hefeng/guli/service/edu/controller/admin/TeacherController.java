bd9a1c82869bdb27056fcdd13e129fc6841ece0f
hee <Michael1117@users.noreply.github.com>
2020-10-23 19:46:44 +0800

    定义统一结果

--- service/service_edu/src/main/java/com/hefeng/guli/service/edu/controller/admin/TeacherController.java
+++ service/service_edu/src/main/java/com/hefeng/guli/service/edu/controller/admin/TeacherController.java
@@ -1,6 +1,7 @@
 package com.hefeng.guli.service.edu.controller.admin;
 
 
+import com.hefeng.guli.common.base.result.R;
 import com.hefeng.guli.service.edu.entity.Teacher;
 import com.hefeng.guli.service.edu.service.TeacherService;
 import io.swagger.annotations.Api;
@@ -29,15 +30,20 @@ public class TeacherController {
 
     @ApiOperation("所有讲师列表")
     @GetMapping("list")
-    public List<Teacher> listAll(){
+    public R listAll(){
         List<Teacher> list = teacherService.list();
-        return list;
+        return R.ok().data("items", list);
     }
 
     @ApiOperation(value = "根据id删除讲师" , notes = "根据id删除讲师，逻辑删除")
     @DeleteMapping("remove/{id}")
-    public boolean removeById(@ApiParam("讲师ID") @PathVariable String id){
-       return teacherService.removeById(id);
+    public R removeById(@ApiParam("讲师ID") @PathVariable String id){
+        boolean result = teacherService.removeById(id);
+        if(result) {
+            return R.ok().message("删除成功");
+        } else {
+            return R.error().message("数据不存在");
+        }
     }
 }
 
