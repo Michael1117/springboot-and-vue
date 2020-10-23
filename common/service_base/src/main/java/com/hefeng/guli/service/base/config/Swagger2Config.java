5de818ce7b545bc233f8f07f8a06ea9dc1b9a8b6
hee <Michael1117@users.noreply.github.com>
2020-10-23 19:20:06 +0800

    swagger 2基本配置

---
+++ common/service_base/src/main/java/com/hefeng/guli/service/base/config/Swagger2Config.java
@@ -0,0 +1,55 @@
+package com.hefeng.guli.service.base.config;
+
+
+import com.google.common.base.Predicates;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class Swagger2Config {
+    @Bean
+    public Docket webApiConfig(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("webApi")
+                .apiInfo(webApiInfo())
+                .select()
+                .paths(Predicates.and(PathSelectors.regex(("/api/.*"))))
+                .build();
+    }
+
+    @Bean
+    public Docket adminApiConfig(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("adminApi")
+                .apiInfo(adminApiInfo())
+                .select()
+                .paths(Predicates.and(PathSelectors.regex(("/admin/.*"))))
+                .build();
+    }
+
+    private ApiInfo webApiInfo(){
+        return new ApiInfoBuilder()
+                .title("网站的API文档")
+                .description("本文档描述了edu的api接口定义")
+                .version("1.0")
+                .contact(new Contact("Michael", "http://heefeng.com", "1234793@qq.com"))
+                .build();
+    }
+
+    private ApiInfo adminApiInfo(){
+        return new ApiInfoBuilder()
+                .title("后台管理系统的API文档")
+                .description("本文档描述了edu后台管理系统的api接口定义")
+                .version("1.0")
+                .contact(new Contact("Michael", "http://heefeng.com", "1234793@qq.com"))
+                .build();
+    }
+}
