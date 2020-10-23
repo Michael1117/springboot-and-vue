25ea963f92d3fa6bd7863abf2a62b167c9431004
hee <Michael1117@users.noreply.github.com>
2020-10-23 22:23:59 +0800

    统一异常处理

---
+++ common/service_base/src/main/java/com/hefeng/guli/service/base/handler/GlobalExceptionHandler.java
@@ -0,0 +1,36 @@
+package com.hefeng.guli.service.base.handler;
+
+
+import com.hefeng.guli.common.base.result.R;
+import com.hefeng.guli.common.base.result.ResultCodeEnum;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.jdbc.BadSqlGrammarException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(Exception.class)
+    @ResponseBody
+    public R error(Exception e){
+        e.printStackTrace();
+        return R.error();
+    }
+
+    @ExceptionHandler(BadSqlGrammarException.class)
+    @ResponseBody
+    public R error(BadSqlGrammarException e){
+
+        e.printStackTrace();
+        return R.setResult(ResultCodeEnum.BAD_SQL_GRAMMAR);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    @ResponseBody
+    public R error(HttpMessageNotReadableException e){
+
+        e.printStackTrace();
+        return R.setResult(ResultCodeEnum.JSON_PARSE_ERROR);
+    }
+}
